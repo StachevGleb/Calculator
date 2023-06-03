8b340985d929d4f2f1e7c2647a872706ec0e4173
Glebcode <gleb.stachev@gmail.com>
2023-06-04 00:59:14 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,286 @@
+let display = document.getElementById('input');
+let btnWrap = document.getElementById('btnWrapper');
+let changeableBtn = document.getElementById('changeableBtn');
+let topBtn = document.getElementById('topBtn');
+let bottomBtn = document.getElementById('bottomBtn');
+let backSpaceBtn = document.getElementById('backSpaceBtn');
+let bracketsBut = document.getElementsByClassName('brackets');
+let counterClick = 0;
+createBtn();
+
+//створення кнопок.
+function createBtn() {
+    for (let i = 0; i < 9; i++) {
+        btnWrap.innerHTML += `<button dataBtn="${i + 1}" id="${i + 1}" class="button${i + 1} button"><p class="pNum">${i + 1}</p></button>`
+    }
+    btnWrap.innerHTML += `<button dataBtn="0" class="button">0</button>`;
+    btnWrap.innerHTML += `<button dataBtn="." id="point" class="button">.</button>`;
+    btnWrap.innerHTML += `<button dataBtn="=" class="buttonEqual" id="resultBtn">=</button>`;
+    topBtn.innerHTML += `<button dataBtn1="CE" class="operBtnStyle ce" id="Ce">CE</button>`;
+    topBtn.innerHTML += `<button class="operBtnStyle addmenu" id="addMenu">...</button>`;
+    topBtn.innerHTML += `<button dataBtn="(" class="brackets operBtnStyle additionalSings button">(</button>`;
+    topBtn.innerHTML += `<button dataBtn=")" class="brackets operBtnStyle additionalSings button">)</button>`;
+    bottomBtn.innerHTML += `<button dataBtn="-" class="operBtnStyle mainSings button" id="minus">-</button>`;
+    bottomBtn.innerHTML += `<button dataBtn="+" class="operBtnStyle mainSings button" id="plus">+</button>`;
+    bottomBtn.innerHTML += `<button dataBtn="÷" class="operBtnStyle mainSings button" id="division">÷</button>`;
+    bottomBtn.innerHTML += `<button dataBtn="×" class="operBtnStyle mainSings button" id="multiplication">×</button>`;
+
+    changeableBtn.innerHTML += `<button dataBtn="%" class="operBtnStyle addOperBtnStyle mainSings button">%</button>`;
+    changeableBtn.innerHTML += `<button dataBtn="√" class="operBtnStyle addOperBtnStyle mainSings button">√</button>`;
+    changeableBtn.innerHTML += `<button dataBtn="²" class="operBtnStyle addOperBtnStyle mainSings button">²</button>`;
+    changeableBtn.innerHTML += `<button dataBtn="π" class="operBtnStyle addOperBtnStyle mainSings button">π</button>`;
+}
+
+let addMenu = document.getElementById('addMenu');
+let operSymbArr = [];
+let operSymbArr2 = [];
+let useBrackets = false;
+
+//зміна операційних кнопок, через кнопку ("...").
+function changeableBtnFunc() {
+    bottomBtn.classList.toggle('hide');
+    changeableBtn.classList.toggle('hide');
+}
+
+addMenu.addEventListener('click', changeableBtnFunc);
+
+//додавання символів на дисплей, якщо, проходять перевірочну функцію writeValidation.
+function displayValue() {
+    let displayNum = this.getAttribute('dataBtn');
+    backSpaceBtn.className = "show";
+    //якщо, пройшов (символ) перевірку writeValidation додати його до вже існуючого текста на дисплей.
+    if (writeValidation(displayNum, display.innerText)) {
+        display.innerText += displayNum;
+    }
+}
+
+let btnValue = document.querySelectorAll('.button');
+
+//метод масива, що при кліку на будь-який .button запускає displayValue.
+btnValue.forEach(function (item) {
+    return item.addEventListener('click', displayValue)
+});
+
+//функція перевірки правильності введення цифр і символів в розрахункове поле.
+function writeValidation(displayNum, displayedVal) {
+    counterClick++
+    // якщо, функція operSymbol вертає параметр displayNum то розбити строку displayedVal через заданий дільник.
+    if (operSymbol(displayNum)) {
+        operSymbArr.push(displayNum)
+    }
+    if (displayNum === ')') {
+        useBrackets = true;
+        return bracketsCheck(displayedVal);
+    }
+    if (operSymbol(displayNum)) {
+        let disArr2 = displayedVal.split('');
+        if (operSymbol((displayNum) && displayNum == '√')) {
+            return true
+        } else if (operSymbol(disArr2[disArr2.length - 1] && displayNum == '√')) {
+            return false
+        }
+        //     }else if (operSymbol(disArr2[disArr2.length - 1])) {
+        //     return false
+        // }
+        // console.log(disArr2.length);
+        // console.log('displayedVal', displayedVal);
+    }
+
+    // якщо, !displayedVal.length 0 то фолс, не записувати натискання на displayNum, крім '√'.
+    if (operSymbol(displayNum) && displayNum == '√') {
+        return true
+    } else if (operSymbol(displayNum) && !displayedVal.length) {
+        return false
+    }
+    // для того, щоб інші кнопки працювали при натисканні.
+    if (!displayedVal.length) {
+        return true
+    }
+    return true
+}
+
+//функція, що виводить на екран кнопки операційні в разі натискання (лише на дані кнопки).
+function operSymbol(displayNum) {
+    return displayNum == '×' || displayNum == '÷' || displayNum == '-' || displayNum == '+' || displayNum == '%' || displayNum == '√' || displayNum == '²' || displayNum == 'π';
+}
+
+
+let deleteCeBtn = document.getElementById('Ce');
+
+// функція, що видаляє повністю весь вміст дисплея.
+function deleteCeFunc() {
+    display.innerText = null;
+    backSpaceBtn.className = "hide";
+}
+
+deleteCeBtn.addEventListener('click', deleteCeFunc)
+
+// функція, що видаляє посимвольно.
+function backSpaceBtnFunc() {
+    let a = display.innerText;
+    let backArr = a.split('');
+    backArr.splice(backArr.length - 1, 1);
+    display.innerText = backArr.join('');
+    if (display.innerText == "") {
+        backSpaceBtn.className = "hide";
+    }
+}
+
+backSpaceBtn.addEventListener('click', backSpaceBtnFunc)
+
+
+let resultBtn = document.getElementById('resultBtn');
+
+resultBtn.addEventListener('click', equalFunction);
+
+//функція equal, що викликає результативну функцію.
+
+function equalFunction() {
+    let displayText = display.innerText;
+    let disArr = displayText.split(/[^\d.]/g);
+    if (useBrackets) {
+        console.log(useBrackets, 'useBrackets');
+        testBracketsFunc(displayText);
+        return
+    }
+    console.log(operSymbArr);
+    // operSymb = disArr.filter(function(item, index, array){
+    // return item == "+" || item == "-" || item == "÷" || item == "×";
+    // console.log(operSymb);
+    // });
+    // console.log(operSymb);
+    resultFunc(disArr, operSymbArr);
+}
+
+function testBracketsFunc(displayText) {
+
+    let test = displayText.split('(')[displayText.split('(').length - 1].split(')');
+    console.log(test, 'test');
+    let test2 = ["6+2)"];
+    console.log("6+2)".split(")"), "split")
+    // console.log(arrDis.indexOf('('))
+}
+
+//функція, що проводить розрахунки введених даних.
+function resultFunc(disArr, operSymbArr) {
+    let resArr = disArr;
+    operSymbArr2 = operSymbArr;
+    let resProp = 0;
+    let multiple = operSymbArr2.indexOf('×');
+    let divide = operSymbArr2.indexOf('÷');
+    let plus = operSymbArr2.indexOf('+');
+    let minus = operSymbArr2.indexOf('-');
+    let sqRoot = operSymbArr2.indexOf('√');
+    let percent = operSymbArr2.indexOf('%');
+    let numberPi = operSymbArr2.indexOf('π');
+    let square = operSymbArr2.indexOf('²');
+
+    console.log("equalFunc", resArr);
+    // console.log(operSymbArr2);
+
+    // console.log(operSymbArr2.indexOf('×'))
+    if (operSymbArr2.length === 0) {
+        display.innerText = disArr[0];
+        return;
+    }
+    if (numberPi >= 0 && operSymbArr2.length !== 0) {
+        resProp = +resArr[numberPi] * 3.1415;
+        resArr.splice(numberPi, 2, resProp);
+        operSymbArr2.splice(numberPi, 1);
+        resultFunc(resArr, operSymbArr2);
+    }
+    if (square >= 0 && operSymbArr2.length !== 0) {
+        resProp = +resArr[square] * +resArr[square];
+        resArr.splice(square, 2, resProp);
+        operSymbArr2.splice(square, 1);
+        resultFunc(resArr, operSymbArr2);
+    }
+    if (percent >= 0 && operSymbArr2.length !== 0) {
+        resProp = ((+resArr[percent] * 0.01) * +resArr[percent - 1]);
+        console.log(percent);
+        resArr.splice(percent, 1, resProp);
+        operSymbArr2.splice(percent, 1);
+        resultFunc(resArr, operSymbArr2);
+    }
+    if (sqRoot >= 0 && operSymbArr2.length !== 0) {
+        resProp = Math.sqrt(resArr[sqRoot + 1]);
+        resArr.splice(sqRoot, 2, resProp);
+        operSymbArr2.splice(sqRoot, 1);
+        resultFunc(resArr, operSymbArr2);
+    }
+    if (multiple >= 0 && operSymbArr2.length !== 0) {
+        resProp = +resArr[multiple] * +resArr[multiple + 1];
+        resArr.splice(multiple, 2, resProp);
+        operSymbArr2.splice(multiple, 1);
+        resultFunc(resArr, operSymbArr2);
+    }
+
+    if (divide >= 0 && operSymbArr2.length !== 0) {
+        resProp = +resArr[divide] / +resArr[divide + 1];
+        resArr.splice(divide, 2, resProp);
+        operSymbArr2.splice(divide, 1);
+        resultFunc(resArr, operSymbArr2);
+    }
+
+    if (plus >= 0 && operSymbArr2.length !== 0) {
+        resProp = +resArr[plus] + +resArr[plus + 1];
+        resArr.splice(plus, 2, resProp);
+        operSymbArr2.splice(plus, 1);
+        resultFunc(resArr, operSymbArr2);
+    }
+
+    if (minus >= 0 && operSymbArr2.length !== 0) {
+        resProp = +resArr[minus] - +resArr[minus + 1];
+        resArr.splice(minus, 2, resProp);
+        operSymbArr2.splice(minus, 1);
+        resultFunc(resArr, operSymbArr2);
+        console.log("hello word");S
+    }
+    // for (let i = 0; operSymbArr.length > i; i++) {
+    //     if (operSymbArr[i] === '×') {
+    //         resProp = +resArr[i] * +resArr[i+1];
+    //         resArr.splice(i, 2, resProp);
+    //         operSymbArr.splice(i, 1);
+    //         resultFunc(resArr, operSymbArr2);
+    //     }
+    //     if (operSymbArr[i] === '÷') {
+    //         resProp = +resArr[i] / +resArr[i+1];
+    //         resArr.splice(i, 2, resProp);
+    //         operSymbArr.splice(i, 1);
+    //         resultFunc(resArr, operSymbArr2);
+    //     }
+    //     if(operSymbArr[i] === '+'){
+    //         resProp = +resArr[i] + +resArr[i+1];
+    //         resArr.splice(i, 2, resProp);
+    //         operSymbArr.splice(i, 1);
+    //         console.log(resArr);
+    //         resultFunc(resArr, operSymbArr2);
+    //     }
+    //     if(operSymbArr[i] === '-'){
+    //         resProp = +resArr[i] - +resArr[i+1];
+    //         resArr.splice(i, 2, resProp);
+    //         operSymbArr.splice(i, 1);
+    //         console.log(resArr);
+    //         resultFunc(resArr, operSymbArr2);
+    //     }
+    // }
+}
+
+//функція перевірки круглих дужок.
+function bracketsCheck(displayedVal) {
+    let disArrBr = displayedVal.split('');
+    let counterLeft = 0;
+    let counterRight = 0;
+    disArrBr.forEach((item) => {
+        if (item === '(') counterLeft++;
+        if (item === ')') counterRight++;
+    })
+    if (disArrBr[disArrBr.length - 1] === '(') {
+        return false;
+    }
+    if (counterLeft > counterRight) {
+        return true;
+    } else {
+        return false;
+    }
+}
